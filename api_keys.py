00e7bc8c4fa213c818bc3b1cd61b98419cf33f7f
cecilializcano <139829754+cecilializcano@users.noreply.github.com>
2023-07-20 21:53:09 -0600

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+import pandas as pd
+import numpy as np
+weather_api_key = "ea49d13b282c2c97287fe6de0669a3bd"
+
+
+# Geoapify API Key
+geoapify_key = "97f8558d9eb5465193ba679bad22f82d"
